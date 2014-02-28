33b905f192871058a690ad67b864a2ab40262a5d
Jaime Visser <jaimevisser@gmail.com>
2014-02-28 23:18:27 +0100

    Test code to read buttons and write to LCD

--- catfeedertimer.ino
+++ catfeedertimer.ino
@@ -1,1 +1,35 @@
+#include <LiquidCrystal.h>
 
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
+int buttonUp = 2;
+int buttonDown = 4;
+int buttonLeft = 3;
+int buttonRight = 5;
+
+void setup()
+{
+  lcd.begin(16,2);
+  lcd.clear();
+  
+  pinMode(buttonUp,INPUT_PULLUP);
+  pinMode(buttonDown,INPUT_PULLUP);
+  pinMode(buttonLeft,INPUT_PULLUP);
+  pinMode(buttonRight,INPUT_PULLUP);
+}
+
+void loop() {
+  lcd.setCursor(0,1);
+  if(digitalRead(buttonUp) == LOW){
+    lcd.write("Up   ");
+  }
+  if(digitalRead(buttonDown) == LOW){
+    lcd.write("Down ");
+  }
+  if(digitalRead(buttonLeft) == LOW){
+    lcd.write("Left ");
+  }
+  if(digitalRead(buttonRight) == LOW){
+    lcd.write("Right");
+  }
+  delay(200);
+}
