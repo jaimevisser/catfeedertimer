3e3ddcc749d0350342d7b06b5b7cb330397d30fa
Jaime Visser <jaimevisser@gmail.com>
2014-03-01 20:31:30 +0100

    Slimmed down code

--- catfeedertimer.ino
+++ catfeedertimer.ino
@@ -1,35 +1,49 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
-int buttonUp = 2;
-int buttonDown = 4;
-int buttonLeft = 3;
-int buttonRight = 5;
+const byte BUTTON_UP    = 2;
+const byte BUTTON_DOWN  = 4;
+const byte BUTTON_LEFT  = 3;
+const byte BUTTON_RIGHT = 5;
+
+const byte BUTTONS[] = {BUTTON_UP,BUTTON_DOWN,BUTTON_LEFT,BUTTON_RIGHT};
+
+const char* BUTTON_NAMES[] = {"UP   ","DOWN ","LEFT ","RIGHT"};
+
+const byte LCD_RS     = 12;
+const byte LCD_ENABLE = 11;
+const byte LCD_D4     = 10;
+const byte LCD_D5     =  9;
+const byte LCD_D6     =  8;
+const byte LCD_D7     =  7;
+
+const byte LCD_COLS = 16;
+const byte LCD_ROWS =  2;
+
+LiquidCrystal lcd(
+  LCD_RS,
+  LCD_ENABLE,
+  LCD_D4,
+  LCD_D5,
+  LCD_D6,
+  LCD_D7
+);
 
 void setup()
 {
-  lcd.begin(16,2);
+  lcd.begin(LCD_COLS, LCD_ROWS);
   lcd.clear();
   
-  pinMode(buttonUp,INPUT_PULLUP);
-  pinMode(buttonDown,INPUT_PULLUP);
-  pinMode(buttonLeft,INPUT_PULLUP);
-  pinMode(buttonRight,INPUT_PULLUP);
+  pinMode(BUTTON_UP, INPUT_PULLUP);
+  pinMode(BUTTON_DOWN, INPUT_PULLUP);
+  pinMode(BUTTON_LEFT, INPUT_PULLUP);
+  pinMode(BUTTON_RIGHT, INPUT_PULLUP);
 }
 
 void loop() {
   lcd.setCursor(0,1);
-  if(digitalRead(buttonUp) == LOW){
-    lcd.write("Up   ");
-  }
-  if(digitalRead(buttonDown) == LOW){
-    lcd.write("Down ");
-  }
-  if(digitalRead(buttonLeft) == LOW){
-    lcd.write("Left ");
-  }
-  if(digitalRead(buttonRight) == LOW){
-    lcd.write("Right");
+  for(byte i=0;i<4;i++){
+    if(digitalRead(BUTTONS[i]) == LOW)
+      lcd.write(BUTTON_NAMES[i]);
   }
   delay(200);
 }
