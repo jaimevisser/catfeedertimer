de91f37e5377ebeab36291c20bc1be84fe96cb96
Jaime Visser <jaimevisser@gmail.com>
2014-03-02 17:29:24 +0100

    Printing time

--- catfeedertimer.ino
+++ catfeedertimer.ino
@@ -1,5 +1,16 @@
 #include <LiquidCrystal.h>
 
+/* Useful Constants */
+#define SECS_PER_MIN  (60UL)
+#define SECS_PER_HOUR (3600UL)
+#define SECS_PER_DAY  (SECS_PER_HOUR * 24UL)
+ 
+/* Useful Macros for getting elapsed time */
+#define numberOfSeconds(_time_) ((_time_) % SECS_PER_MIN)  
+#define numberOfMinutes(_time_) (((_time_) / SECS_PER_MIN) % SECS_PER_MIN) 
+#define numberOfHours(_time_) (((_time_) % SECS_PER_DAY) / SECS_PER_HOUR)
+#define elapsedDays(_time_) ((_time_) / SECS_PER_DAY)
+
 const byte BUTTON_UP    = 2;
 const byte BUTTON_DOWN  = 4;
 const byte BUTTON_LEFT  = 3;
@@ -18,6 +29,9 @@ const byte LCD_D7     =  7;
 
 const byte LCD_COLS = 16;
 const byte LCD_ROWS =  2;
+
+unsigned long offset = 00 + (06*SECS_PER_MIN) + (17*SECS_PER_HOUR);
+unsigned long timer[4] = {200,0,0,0};
 
 LiquidCrystal lcd(
   LCD_RS,
@@ -40,10 +54,31 @@ void setup()
 }
 
 void loop() {
-  lcd.setCursor(0,1);
-  for(byte i=0;i<4;i++){
-    if(digitalRead(BUTTONS[i]) == LOW)
-      lcd.write(BUTTON_NAMES[i]);
-  }
-  delay(200);
+  printmain();
+}
+
+void printmain(){
+ lcd.setCursor(0,0);
+ for(byte i=0;i<4;i++){
+   if(timer[i] > 0){
+     lcd.print('O');
+   }else{
+     lcd.print('*');
+   }
+ }
+ 
+ unsigned long time = (millis() / 1000UL) + offset;
+ 
+ lcd.print(elapsedDays(time));
+ lcd.print('/');
+ printPad(numberOfHours(time));
+ lcd.print(':'); 
+ printPad(numberOfMinutes(time));
+ lcd.print(':');
+ printPad(numberOfSeconds(time));
+}
+
+void printPad(byte t){
+  if(t < 10) lcd.print("0");
+  lcd.print(t);
 }
