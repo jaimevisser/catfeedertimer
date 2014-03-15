fb678149d11186f96f340d5067a9e143a0e95835
Jaime Visser <jaimevisser@gmail.com>
2014-03-15 21:33:14 +0100

    Changed menu layout

--- catfeedertimer.ino
+++ catfeedertimer.ino
@@ -45,11 +45,24 @@ unsigned long timer[4] = {
   (12*SECS_PER_HOUR)+(2*SECS_PER_DAY),
   (12*SECS_PER_HOUR)+(3*SECS_PER_DAY)};
 
+const unsigned long TIMER_MAX = (9 * SECS_PER_DAY)+(23 * SECS_PER_HOUR)+(60 * SECS_PER_MIN);
 const byte STATE_SELECT = 5;
 const byte STATE_CLOCK  = 4;
+const byte LAST_STATE   = 5;
 
 byte state = 0;
 byte selected = 0;
+
+const byte CHAR_ARROW_DOWN = 0;
+byte CHAR_ARROW_DOWN_DATA[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B10001,
+  B01010,
+  B00100
+};
 
 LiquidCrystal lcd(
 LCD_RS,
@@ -66,6 +79,8 @@ void setup()
 {
   lcd.begin(LCD_COLS, LCD_ROWS);
   lcd.clear();
+
+  lcd.createChar(0, CHAR_ARROW_DOWN_DATA);
 
   servo.attach(SERVO);
 
@@ -104,25 +119,49 @@ void afterButton(){
 }
 
 void onUp(){
-  if(state < 4 && selected < 2){
-    timer[state] += timeincrement[selected];
-    printBottomLine();
+  if(selected < 3){
+    if(state < 4){
+      if((timer[state] + timeincrement[selected]) < TIMER_MAX){
+        timer[state] += timeincrement[selected];
+        printBottomLine();
+      }
+    }
+    else if(state == 4){
+      offset += timeincrement[selected];
+      printBottomLine();
+    }
   }
-  else if(state < 5 && selected == 3){
-    selected = state;
-    state = STATE_SELECT;
+  else if(state > 0){
+    state--;
+    printBoth();
+  }
+  else if(state == 0){
+    state=LAST_STATE;
     printBoth();
   }
 }
 
 void onDown(){
-  if((state < 4) && (timer[state] >= timeincrement[selected])){
-    timer[state] -= timeincrement[selected];
-    printBottomLine();
+  if(selected < 3){
+    if(state < 4){
+      if(timer[state] >= timeincrement[selected]){
+        timer[state] -= timeincrement[selected];
+        printBottomLine();
+      }
+    }
+    else if(state == 4){
+      if(offset >= timeincrement[selected]){
+        offset -= timeincrement[selected];
+        printBottomLine();
+      }
+    }
   }
-  else if(state == 5){
-    state = selected;
-    selected = 0;
+  else if(state < LAST_STATE){
+    state++;
+    printBoth();
+  }
+  else if(state == LAST_STATE){
+    state = 0;
     printBoth();
   }
 }
@@ -135,22 +174,15 @@ void onLeft(){
 }
 
 void onRight(){
-  if(state < 5){
-    if(selected < 3){
-      selected += 1;
-      printBottomLine();
-    }
-  }
-  else if(state == 5){
-    if(selected < 4){
-      selected += 1;
-      printBottomLine();
-    }
+  if(selected < 3){
+    selected += 1;
+    printBottomLine();
   }
 }
 
 void printBoth(){
   lcd.noCursor();
+  lcd.clear();
   printtl();
   printbl();
   setcursor();
@@ -171,35 +203,49 @@ void printBottomLine(){
 
 void printtl(){
   lcd.setCursor(0,0);
-  for(byte i=0;i<4;i++){
-    if(i == state){
-      lcd.print('*');
+  if(state < 5){
+    if(state < 4){
+      lcd.print("Inst. Vak ");
+      lcd.print(state+1);
+      lcd.setCursor(15,0);
+      lcd.print("^");
     }
-    else if(timer[i] > 0){
-      lcd.print('O');
-    }
-    else{
-      lcd.print('.');
+    else if(state == 4){
+      lcd.print("Inst. Klok");
+      lcd.setCursor(15,0);
+      lcd.print("^");
     }
   }
-  
-  lcd.print('C');
+  else{
+    for(byte i=0;i<4;i++){
+      if(i == state){
+        lcd.print('*');
+      }
+      else if(timer[i] > 0){
+        lcd.print('O');
+      }
+      else{
+        lcd.print('.');
+      }
+    }
+  }
+
 
 }
 
 void printbl(){
-  lcd.setCursor(1,1);
+  lcd.setCursor(0,1);
 
   unsigned long time; 
-  if(state < 5){
+  if(state < 4){
     time = timer[state];
   }
   else{
     time = (millis() / 1000UL) + offset;
   }
 
-  lcd.print("dag ");
-  printPad(elapsedDays(time));
+  lcd.print("d");
+  lcd.print(elapsedDays(time));
   lcd.print('/');
   printPad(numberOfHours(time));
   lcd.print(':'); 
@@ -208,17 +254,19 @@ void printbl(){
     lcd.print(':');
     printPad(numberOfSeconds(time));
   }
-  else{
-    lcd.print(" ^");
-  }
+
+  lcd.setCursor(15,1);
+  lcd.write(CHAR_ARROW_DOWN);
 }
 
 void setcursor(){
   if(state < 5){
-    lcd.setCursor(5+(selected*3),1);
-  }
-  else if(state == STATE_SELECT){
-    lcd.setCursor(selected,0);
+    if(selected < 3){
+      lcd.setCursor(1+(selected*3),1);
+    }
+    else{
+      lcd.setCursor(15,1);
+    }
   }
 
   if(state < 6){
@@ -230,6 +278,13 @@ void printPad(byte t){
   if(t < 10) lcd.print("0");
   lcd.print(t);
 }
+
+
+
+
+
+
+
 
 
 
